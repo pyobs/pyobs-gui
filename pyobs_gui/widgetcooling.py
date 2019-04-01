dc70be0236a3416e7e0e2dcbb73973844c96923d
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-04-01 19:33:39 +0200

    cooling display works

--- pyobs_gui/widgetcooling.py
+++ pyobs_gui/widgetcooling.py
@@ -1,26 +1,48 @@
-import threading
 import logging
-from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSignal
 
 from pyobs.interfaces import ICooling
+from pyobs_gui.basewidget import BaseWidget
 from .qt.widgetcooling import Ui_WidgetCooling
 
 
 log = logging.getLogger(__name__)
 
 
-class WidgetCooling(QtWidgets.QWidget, Ui_WidgetCooling):
+class WidgetCooling(BaseWidget, Ui_WidgetCooling):
     signal_update_gui = pyqtSignal()
 
     def __init__(self, module, comm, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
+        BaseWidget.__init__(self, parent=parent, update_func=self._update)
         self.setupUi(self)
         self.module = module    # type: ICooling
         self.comm = comm        # type: Comm
 
-    def enter(self):
-        pass
+        # status
+        self._status = None
 
-    def leave(self):
-        pass
+        # connect signals
+        self.signal_update_gui.connect(self.update_gui)
+
+    def _update(self):
+        # get status
+        self._status = self.module.get_cooling_status()
+
+        # signal GUI update
+        self.signal_update_gui.emit()
+
+    def update_gui(self):
+        if self._status is not None:
+            # enable myself
+            self.setEnabled(True)
+
+            # split values
+            enabled, set_point, power, temps = self._status
+
+            # set it
+            if enabled:
+                self.labelStatus.setText('Set=%.1f°C' % set_point)
+                self.labelPower.setText('%d%%' % power)
+            else:
+                self.labelStatus.setText('OFF')
+                self.labelStatus.clear()
