d5d7253c4281b01937d05160774b7ee386a0eed3
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-04-08 18:31:50 +0200

    changed version number to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='pyobs_gui',
-    version='0.1',
+    version='0.2',
     description='GUI for pyobs',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
