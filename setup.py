7a7df698e3a08b83b358ece71c1395396c584e30
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-11-17 15:23:26 +0100

    catching ModuleNotFoundError for pwd on Windows

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyobs-gui',
-    version='0.8.1',
+    version='0.8.2',
     description='GUI for pyobs',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
