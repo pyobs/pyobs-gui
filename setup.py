08f697ea767b6c0a0e5e1d2f58c3f82486199fb4
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-11-17 14:34:52 +0100

    v0.8

--- setup.py
+++ setup.py
@@ -2,10 +2,22 @@
 from distutils.core import setup
 
 setup(
-    name='pyobs_gui',
-    version='0.2',
+    name='pyobs-gui',
+    version='0.8',
     description='GUI for pyobs',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
-    packages=['pyobs_gui', 'pyobs_gui.qt']
+    packages=[
+        'pyobs_gui',
+        'pyobs_gui.qt'
+    ],
+    install_requires=[
+        'astroquery',
+        'astroplan',
+        'colour',
+        'PyQt5',
+        'astropy',
+        'aplpy',
+        'matplotlib'
+    ]
 )
