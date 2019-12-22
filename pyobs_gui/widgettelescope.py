43af1a7f98ba9f0a8921af53c64d2bfd116c6afc
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-12-22 08:16:45 +0100

    converting np.float to float

--- pyobs_gui/widgettelescope.py
+++ pyobs_gui/widgettelescope.py
@@ -129,7 +129,7 @@ class WidgetTelescope(BaseWidget, Ui_WidgetTelescope):
         coords = SkyCoord(ra + ' ' + dec, frame=ICRS, unit=(u.hour, u.deg))
 
         # start thread with move
-        self.run_async(self.module.track_radec, coords.ra.degree, coords.dec.degree)
+        self.run_async(self.module.track_radec, float(coords.ra.degree), float(coords.dec.degree))
 
         # plot it
         #self.plot.plot(coords)
