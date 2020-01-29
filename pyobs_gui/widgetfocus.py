0ae82a6d07cc5543e64d93bfac3da3b91e8b4939
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-01-29 22:03:35 +0200

    new way of setting focus via input dialog

--- pyobs_gui/widgetfocus.py
+++ pyobs_gui/widgetfocus.py
@@ -23,19 +23,40 @@ class WidgetFocus(BaseWidget, Ui_WidgetFocus):
 
         # variables
         self._focus = None
+        self._focus_offset = None
         self._motion_status = IMotion.Status.UNKNOWN
 
         # connect signals
         self.signal_update_gui.connect(self.update_gui)
-        self.butSetFocus.clicked.connect(lambda: self.run_async(self.module.set_focus,
-                                                                self.spinFocus.value()))
+        self.butSetFocusBase.clicked.connect(lambda: self._set_focus(False))
+        self.butSetFocusOffset.clicked.connect(lambda: self._set_focus(True))
 
         # subscribe to events
         self.comm.register_event(MotionStatusChangedEvent, self._on_motion_status_changed)
 
+    def _set_focus(self, offset: bool = False):
+        """Asks user for new focus (offset) and sets it.
+
+        Args:
+            offset: If False, base focus is set, otherwise offset.
+        """
+
+        # base of offset?
+        title = 'Focus offset' if offset else 'Focus'
+        value = self._focus_offset if offset else self._focus
+        minval = -5 if offset else 0
+        maxval = 5 if offset else 100
+        setter = self.module.set_focus_offset if offset else self.module.set_focus
+
+        # ask for value
+        new_value, ok = QtWidgets.QInputDialog.getDouble(self, title, 'New value', value, minval, maxval, 2)
+        if ok:
+            self.run_async(setter, new_value)
+
     def _init(self):
         # get current filter
         self._focus = self.module.get_focus().wait()
+        self._focus_offset = self.module.get_focus_offset().wait()
         self._motion_status = self.module.get_motion_status().wait()
         self.signal_update_gui.emit()
 
@@ -43,7 +64,10 @@ class WidgetFocus(BaseWidget, Ui_WidgetFocus):
         # enable myself and set filter
         self.setEnabled(True)
         self.labelCurStatus.setText(self._motion_status.name)
-        self.labelCurFocus.setText('' if self._focus is None else '%.3f' % self._focus)
+        self.labelCurFocusBase.setText('' if self._focus is None else '%.3f' % self._focus)
+        self.labelCurFocusOffset.setText('' if self._focus_offset is None else '%.3f' % self._focus_offset)
+        self.labelCurFocus.setText('' if self._focus is None or self._focus_offset is None
+                                   else '%.3f' % (self._focus + self._focus_offset,))
 
     def _on_motion_status_changed(self, event: MotionStatusChangedEvent, sender: str):
         """Called when motion status of module changed.
@@ -69,6 +93,7 @@ class WidgetFocus(BaseWidget, Ui_WidgetFocus):
     def _update(self):
         # get focus and motion status
         self._focus = self.module.get_focus().wait()
+        self._focus_offset = self.module.get_focus_offset().wait()
         self._motion_status = self.module.get_motion_status().wait()
 
         # signal GUI update
