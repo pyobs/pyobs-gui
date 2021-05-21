501ff9b96f553527b0ab82314df9d059a3896103
Tim-Oliver Husser <thusser@uni-goettingen.de>
2021-05-21 18:03:15 +0200

    moved requirements into requirements.txt

--- setup.py
+++ setup.py
@@ -7,17 +7,6 @@ setup(
     description='GUI for pyobs',
     author='Tim-Oliver Husser',
     author_email='thusser@uni-goettingen.de',
-    packages=[
-        'pyobs_gui',
-        'pyobs_gui.qt'
-    ],
-    install_requires=[
-        'astroquery',
-        'astroplan',
-        'colour',
-        'PyQt5==5.13',
-        'astropy',
-        'matplotlib',
-        'qfitswidget'
-    ]
+    packages=['pyobs_gui', 'pyobs_gui.qt'],
+    install_requires=[line.strip() for line in open("requirements.txt").readlines()]
 )
