d82ae878dfcea9687b6bb6aea2e322e83ba10086
Tim-Oliver Husser <thusser@uni-goettingen.de>
2022-06-16 12:04:47 +0200

    removed unnecessary plugins

--- pyobs_gui/qt/camerawidget.py
+++
@@ -1,7 +0,0 @@
-from PyQt5 import QtWidgets, uic
-
-
-class CameraWidget(QtWidgets.QWidget):
-    def __init__(self, *args, **kwargs):
-        QtWidgets.QWidget.__init__(self, *args, **kwargs)
-        uic.loadUi("camerawidget.ui", self)
