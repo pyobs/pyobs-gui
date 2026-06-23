c3348323ebf8101ccb6379ad0fe5248d6da00260
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-06-23 08:27:26 +0200

    CoolingWidget working

--- pyobs_gui/coolingwidget.py
+++ pyobs_gui/coolingwidget.py
@@ -18,8 +18,7 @@ class CoolingWidget(BaseWidget, Ui_CoolingWidget):
         BaseWidget.__init__(self, **kwargs)
         self.setupUi(self)  # type: ignore
 
-        # status
-        self._status: tuple[bool, float, float] | None = None
+        # state
         self.state: CoolingState | None = None
 
         # connect signals
@@ -28,8 +27,9 @@ class CoolingWidget(BaseWidget, Ui_CoolingWidget):
         self.buttonApply.clicked.connect(self.buttonApply_clicked)
 
     async def _init(self) -> None:
-        print(self.module)
+        print("_init", self.module)
         await self.comm.subscribe_state(self.module, ICooling, self._update_state)
+        print("done")
 
     def _update_state(self, state: CoolingState) -> None:
         print("_update_state")
@@ -37,16 +37,14 @@ class CoolingWidget(BaseWidget, Ui_CoolingWidget):
         self.signal_update_gui.emit()
 
     def update_gui(self) -> None:
-        if self._status is not None:
-            # enable myself
+        if self.state is not None:
             self.setEnabled(True)
 
-            # set it
-            if self.state and self.state.enabled:
-                self.labelStatus.setText("N/A" if self.state.setpoint is None else f"{self.state.setpoint:.1}f°C")
-                self.labelPower.setText("N/A" if self.state.power is None else f"{self.state.power:d}%%")
+            if self.state.enabled:
+                self.labelStatus.setText("N/A" if self.state.setpoint is None else f"{self.state.setpoint:.1f}°C")
+                self.labelPower.setText("N/A" if self.state.power is None else f"{self.state.power:d}%")
             else:
-                self.labelStatus.setText("N/A" if self.state is None or self.state.power is None else "OFF")
+                self.labelStatus.setText("OFF")
                 self.labelPower.clear()
 
     @QtCore.Slot(bool)  # type: ignore
@@ -55,11 +53,7 @@ class CoolingWidget(BaseWidget, Ui_CoolingWidget):
 
     @qasync.asyncSlot()  # type: ignore
     async def buttonApply_clicked(self) -> None:
-        # get enabled and setpoint temperature
         enabled = self.checkEnabled.isChecked()
         temp = self.spinSetPoint.value()
-
-        # send it
-        module = self.module
-        if isinstance(module, ICooling):
-            await module.set_cooling(enabled, temp)
+        async with self.comm.proxy(self.module, ICooling) as proxy:
+            await proxy.set_cooling(enabled, temp)
