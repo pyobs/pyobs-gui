39b1e61e8eacd61c3ff34aba63ec5bb674eaf949
Tim-Oliver Husser <thusser@uni-goettingen.de>
2026-06-29 12:23:51 +0200

    refactor: migrate ModeWidget to use capability-based mode handling and state subscriptions, improve UI logic and maintain backward compatibility

--- pyobs_gui/modewidget.py
+++ pyobs_gui/modewidget.py
@@ -44,14 +44,14 @@ class ModeWidget(BaseWidget, Ui_ModeWidget):
     async def _init(self) -> None:
         await self.comm.subscribe_state(self.module, IMotion, self._on_motion_state)
 
-        if await self.comm.has_proxy(self.module, IMode):
-            async with self.comm.proxy(self.module, IMode) as proxy:
-                self._mode_groups = await proxy.list_mode_groups()
-                self._mode_options = [await proxy.list_modes(i) for i in range(len(self._mode_groups))]
-                self._modes = [await proxy.get_mode(i) for i in range(len(self._mode_groups))]
+        # read capabilities (static: available modes per group)
+        caps = await self.comm.get_capabilities(self.module, IMode)
+        if caps is not None:
+            self._mode_groups = list(caps.modes.keys())
+            self._mode_options = [caps.modes[g] for g in self._mode_groups]
 
             self._mode_widgets = []
-            for i in range(len(self._mode_groups)):
+            for i, group in enumerate(self._mode_groups):
                 layout = QtWidgets.QHBoxLayout()
                 current = QtWidgets.QLineEdit()
                 current.setReadOnly(True)
@@ -63,7 +63,10 @@ class ModeWidget(BaseWidget, Ui_ModeWidget):
                 button.clicked.connect(functools.partial(self.set_mode, i))
                 layout.addWidget(button)
                 self._mode_widgets.append((current, button))
-                cast("QtWidgets.QFormLayout", self.groupBox.layout()).addRow(self._mode_groups[i], layout)
+                cast("QtWidgets.QFormLayout", self.groupBox.layout()).addRow(group, layout)
+
+            # subscribe to live mode state
+            await self.comm.subscribe_state(self.module, IMode, self._on_mode_state)
 
         self.signal_update_gui.emit()
 
@@ -84,13 +87,18 @@ class ModeWidget(BaseWidget, Ui_ModeWidget):
             self._mode_widgets[i][0].setText(self._modes[i])
             self._mode_widgets[i][1].setEnabled(initialized)
 
+    def _on_mode_state(self, state: IMode.State) -> None:
+        self._modes = [state.modes.get(g, "") for g in self._mode_groups]
+        self.signal_update_gui.emit()
+
     async def _on_mode_changed(self, event: Event, sender: str) -> bool:
+        """Keep for backwards compat with modules that still send ModeChangedEvent."""
         if sender != self.module or not isinstance(event, ModeChangedEvent):
             return False
-
-        g = self._mode_groups.index(event.group)
-        self._modes[g] = event.mode
-        self.signal_update_gui.emit()
+        if event.group in self._mode_groups:
+            g = self._mode_groups.index(event.group)
+            self._modes[g] = event.mode
+            self.signal_update_gui.emit()
         return True
 
     @QtCore.Slot(int)  # type: ignore
@@ -100,6 +108,7 @@ class ModeWidget(BaseWidget, Ui_ModeWidget):
             self, f"Set {mode}", f"New {mode}", self._mode_options[group], 0, False
         )
         if ok:
+
             async def _do_set_mode() -> None:
                 async with self.comm.proxy(self.module, IMode) as proxy:
                     await proxy.set_mode(new_value, group)
